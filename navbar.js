7935e5cc5247d9a63a023f9d69a9265c63436cb3
DanTec007 <danygiorivero@gmail.com>
2025-04-06 16:36:15 -0400

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,1 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const navbar = `
+    <header>
+      <nav class="navbar">
+        <a href="index.html">
+          <img src="images/logo.jpg" alt="Dan's Bakery Logo" class="logo">
+        </a>
+        <ul class="nav-links">
+          <li><a href="index.html">Home</a></li>
+          <li><a href="about.html">About</a></li>
+          <li><a href="products.html">Products</a></li>
+          <li><a href="promotions.html">Promotions</a></li>
+          <li><a href="delivery.html">Delivery</a></li>
+          <li><a href="faq.html">FAQ</a></li>
+          <li><a href="reviews.html" class="active">Reviews</a></li>
+          <li><a href="contact.html">Contact</a></li>
+          <li><a href="signup.html">Sign Up</a></li>
+          <li><a href="order.html">Order Now</a></li>
+        </ul>
+      </nav>
+    </header>
+  `;
+  document.getElementById("navbar-container").innerHTML = navbar;
+});
 
